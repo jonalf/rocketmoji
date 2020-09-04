a0af8be3f7b19efbb8808aaa1fde2695b6e6b1b9
JonAlf <jonalf@gmail.com>
2020-09-04 14:04:04 -0400

    +file

---
+++ rocketmoji.py
@@ -0,0 +1,85 @@
+XAUTHTOKEN = "foo"
+XUSERID = "bar"
+
+"""
+use emoji yaml files like the ones found here:
+https://github.com/lambtron/emojipacks
+"""
+
+"""
+Example curl API call to be replicated
+#/bin/bash
+curl -H "X-Auth-Token: " \
+     -H "X-User-Id: " \
+     http://localhost:3000/api/v1/emoji-custom.list
+
+
+curl -H "X-Auth-Token: " \
+     -H "X-User-Id: " \
+     -F "emoji=@7zYM751.png" \
+     -F "name=bender" \
+     -F "alias=" \
+     http://localhost:3000/api/v1/emoji-custom.create
+"""
+
+import yaml
+import os
+from urllib.request import urlopen, urlretrieve
+
+
+#retrieve emoji yaml file
+def get_emoji_yaml( emoji_url ):
+    emoji_response = urlopen(emoji_url)
+    emoji_yaml = yaml.load(emoji_response.read())
+    return emoji_yaml
+
+def batch_save_emojis( emoji_yaml ):
+    #make dir for temp saving files
+    tmpdir = "/tmp/" + emoji_yaml['title']
+    os.mkdir(tmpdir)
+
+    emojis = emoji_yaml['emojis']
+    print(emojis)
+    for emoji in emojis:
+        name = emoji['name']
+        url = emoji['src']
+        filename = name + url[url.rfind('.'):]
+        # print(name)
+        # print(url)
+        # print(filename)
+        filename = tmpdir + '/' + filename
+        urlretrieve(url, filename)
+        emoji_create_api_call(filename, name)
+
+    #get rid of emoji files
+    os.system('rm -rf ' + tmpdir)
+
+'''
+ideally, should be able to make API call using requests.
+Have not succedded, should look something like:
+
+headers = {'X-Auth-Token': '',
+           'X-User-Id': ''}
+data = {'emoji': open('bender.png', 'rb'),
+        'name': 'bender' }
+r = requests.post('http://localhost:3000/api/v1/emoji-custom.create', data=data, headers = headers)
+r.txt
+
+until that is working, using os.system instead
+'''
+
+def emoji_create_api_call(emojifile, emojiname):
+    cmd = 'curl -H "X-Auth-Token: %s" '%XAUTHTOKEN
+    cmd+= '-H "X-User-Id: %s" '%XUSERID
+    cmd+= '-F "emoji=@'
+    cmd+= emojifile + '" '
+    cmd+= '-F "name=' + emojiname + '" '
+    cmd+= 'http://localhost:3000/api/v1/emoji-custom.create'
+    print(emojiname)
+    print(cmd)
+    print()
+    #os.system(cmd)
+
+emoji_yaml_url = input("URL for YAML file?")
+emoji_yaml = get_emoji_yaml(emoji_yaml_url)
+batch_save_emojis( emoji_yaml )
